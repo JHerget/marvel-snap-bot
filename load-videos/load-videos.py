f6c6def2b5cc93792fcbbec038562d37de2ae4ff
Jordan Herget <lethalbacon16@gmail.com>
2023-07-27 00:12:13 -0600

    work on layout mapping

--- load-videos/load-videos.py
+++ load-videos/load-videos.py
@@ -15,10 +15,29 @@ video = cv2.VideoCapture(url)
 fps = video.get(cv2.CAP_PROP_FPS)
 start_frame = int((60*start_min + start_sec)*fps)
 
-# video.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
+video.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
 success, frame = video.read()
-
 print(success)
 
-print(cv2.CAP_PROP_FRAME_COUNT)
-print(cv2.CAP_PROP_POS_FRAMES)
+height, width, channels = frame.shape
+row_start = int(float(config["hoogland_layout"]["locations"]["row_start"])*height)
+row_end = int(float(config["hoogland_layout"]["locations"]["row_end"])*height)
+col_start = int(float(config["hoogland_layout"]["locations"]["col_start"])*width)
+col_end = int(float(config["hoogland_layout"]["locations"]["col_end"])*width)
+locations = frame[row_start:row_end, col_start:col_end]
+
+cv2.imshow("frame", locations)
+cv2.waitKey(5000)
+
+# image = cv2.imread("test.png")
+# height, width, channels = image.shape
+#
+# row_start = int(float(config["hoogland_layout"]["locations"]["row_start"])*height)
+# row_end = int(float(config["hoogland_layout"]["locations"]["row_end"])*height)
+# col_start = int(float(config["hoogland_layout"]["locations"]["col_start"])*width)
+# col_end = int(float(config["hoogland_layout"]["locations"]["col_end"])*width)
+# locations = image[row_start:row_end, col_start:col_end]
+#
+# cv2.imshow("testimage", locations)
+# cv2.waitKey(10000)
+# print(image.shape)
